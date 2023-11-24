34fc2c5d6296ec9dcced703cfa77882e294c78f5
ahamadey27 <hamadey@gmail.com>
2023-11-24 15:42:48 -0500

    C#

--- Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
+++ Basic_C#_Programs/myConsoleProject.cs/myConsoleProject.cs/Program.cs
@@ -1,15 +1,15 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace myConsoleProject.cs
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            Console.WriteLine("What is your name?");
+            String name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + "!");
+            Console.Read();
         }
     }
 }
---
+++ Basic_C#_Programs/program.cs
@@ -0,0 +1,10 @@
+using System; //connects the library in the .NET framework
+
+class Program //grouping of code put together
+{
+	static void Main() //static void modifieds Main method
+	{
+		Console.WriteLine("Hello, world");
+	}
+
+}
