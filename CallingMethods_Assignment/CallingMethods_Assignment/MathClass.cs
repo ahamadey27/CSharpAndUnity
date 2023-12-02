d4f81db4ad2ae6d6589610138837e313722774d0
ahamadey27 <hamadey@gmail.com>
2023-12-02 15:49:55 -0500

    CallingMethodAssignment_Complete
    Simple use of creating a new class and adding methods that main program refers to after taking in user input

--- CallingMethods_Assignment/CallingMethods_Assignment/MathClass.cs
+++ CallingMethods_Assignment/CallingMethods_Assignment/MathClass.cs
@@ -4,9 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+//
 namespace CallingMethods_Assignment
 {
-    class MathClass
+    public class MathClass //make this public 
     {
+        public int Square(int number)
+        {
+            return number * number;
+        }
+
+        public int AddTen(int number)
+        {
+            return number + 10;
+        }
+
+        public int SubtractOne(int number)
+        {
+            return number - 1;
+        }
     }
 }
