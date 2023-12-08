8a967eaa3c80fbf5813828c753a48e5a6b9c1375
ahamadey27 <hamadey@gmail.com>
2023-12-08 18:39:11 -0500

    21Game_Misc

--- TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
+++ TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
@@ -12,22 +12,22 @@ namespace TwentyOneGame_Classes_And_Objects
         {
             //List<Game> games = new List<Game>();
             //Game game = new TwentyOneGame(); //example of polymorphism...ability of a class to morph into its inheriting class to give certain advantages
-            TwentyOneGame game = new TwentyOneGame();
-            game.Players = new List<string>() { "hammer", "bill", "sam" };
-            game.ListPlayers();
-            Console.ReadLine(); 
+            //TwentyOneGame game = new TwentyOneGame();
+            //game.Players = new List<string>() { "hammer", "bill", "sam" };
+            //game.ListPlayers();
+            //Console.ReadLine(); 
             //games.Add(game); 
 
 
-            //Deck deck = new Deck();
-            //deck.Shuffle(3);
+            Deck deck = new Deck();
+            deck.Shuffle(3);
 
-            //foreach (Card card in deck.Cards)
-            //{
-            //    Console.WriteLine(card.Face + " of " + card.Suit);
-            //}
-            //Console.WriteLine(deck.Cards.Count);
-            //Console.Read();
+            foreach (Card card in deck.Cards)
+            {
+                Console.WriteLine(card.Face + " of " + card.Suit);
+            }
+            Console.WriteLine(deck.Cards.Count);
+            Console.Read();
         }
     }
 }
