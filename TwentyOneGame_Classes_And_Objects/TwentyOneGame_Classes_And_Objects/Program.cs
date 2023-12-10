fac657ede0103da79b23145e6da5dc7a9c811c17
ahamadey27 <hamadey@gmail.com>
2023-12-10 12:52:52 -0500

    21Game_Examlpe of Generics

--- TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
+++ TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
@@ -10,12 +10,7 @@ namespace TwentyOneGame_Classes_And_Objects
     {
         static void Main(string[] args)
         {
-            Game game = new TwentyOneGame();
-            game.Players = new List<Player>(); 
-            Player player = new Player();
-            player.Name = "hammer";
-            game += player;
-            game -= player;
+            Player<Card> player = new Player<Card>(); 
             Deck deck = new Deck();
             deck.Shuffle(3);
 
