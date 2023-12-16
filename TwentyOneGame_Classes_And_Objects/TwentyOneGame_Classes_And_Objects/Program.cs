9f5bb9655ab783d4a5409a9732697868bcbac379
ahamadey27 <hamadey@gmail.com>
2023-12-16 13:01:14 -0500

    21Game_Lambda Expresions
    Check Program.cs

--- TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
+++ TwentyOneGame_Classes_And_Objects/TwentyOneGame_Classes_And_Objects/Program.cs
@@ -11,13 +11,34 @@ namespace TwentyOneGame_Classes_And_Objects
         static void Main(string[] args)
         {        
             Deck deck = new Deck();
-            deck.Shuffle(3);
 
-            foreach (Card card in deck.Cards)
-            {
-                Console.WriteLine(card.Face + " of " + card.Suit);
-            }
-            Console.WriteLine(deck.Cards.Count);
+            //Example 1
+            //int count = deck.Cards.Count(x => x.Face == Face.Ace); //basic lamda function that counts Aces..count is the lambda function 
+            // => means evaluate this on each item 
+            //Console.WriteLine(count); 
+
+            //Example 2
+            //List<Card> newList = deck.Cards.Where(x => x.Face == Face.King).ToList(); //where is the lamda function here 
+            //foreach (Card card in newList)
+            //{
+            //    Console.WriteLine(card.Face); 
+            //}
+
+            //Example 3
+            List<int> numberList = new List<int>() { 1, 2, 3, 535, 235 };
+            //int sum = numberList.Sum(x => x + 5); //adds five to each number 
+            //int sum = numberList.Min(); //simple way to find smallest number 
+            int sum = numberList.Where(x => x > 20).Sum(); //create a list where the numbers are greater than 20
+            Console.WriteLine(sum); 
+
+            
+            //deck.Shuffle(3);
+
+            //foreach (Card card in deck.Cards)
+            //{
+            //    Console.WriteLine(card.Face + " of " + card.Suit);
+            //}
+            //Console.WriteLine(deck.Cards.Count);
             Console.Read();
         }
     }
