6d7d80323295f812cf58f8849d49a360eb19d7fd
ahamadey27 <hamadey@gmail.com>
2023-12-28 15:25:26 -0500

    Finite State Machine Scripts
    Refer to the Player States folder scripts that replaces what PlayerController.cs does. Note PlayerController_FSM.cs refers to the five player state scripts

---
+++ Finite_State_Machines/Player States/PlayerIdleState.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerIdleState : PlayerBaseState //got here by right click and select QuickActions of PlayerIdleState after changing Monobehavior to PlayerBaseState 
+{
+    public override void EnterState(PlayerController_FSM player)
+    {
+        player.SetExpression(player.idleSprite); 
+    }
+
+    public override void OnCollisionEnter(PlayerController_FSM player)
+    {
+       
+    }
+
+    public override void Update(PlayerController_FSM player)
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            player.Rigidbody.AddForce(Vector3.up * player.jumpForce);
+            player.TransitionToState(player.JumpingState); 
+        }
+
+        if(Input.GetButtonDown("Duck"))
+        {
+            player.TransitionToState(player.DuckingState); 
+        }
+
+        if (Input.GetButtonDown("SwapWeapon"))
+        {
+            bool usingWeapon01 = player.weapon01.gameObject.activeInHierarchy;
+
+            player.weapon01.gameObject.SetActive(usingWeapon01 == false);
+            player.weapon02.gameObject.SetActive(usingWeapon01); 
+        }
+    }
+       
+}
